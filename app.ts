f8b83c389734536b0c2301795ce7d1031fc8e44d
JohnnyOak85 <joao.cmp@proton.me>
2022-09-27 22:56:01 +0100

    Ensuring new users have base player role

--- app.ts
+++ app.ts
@@ -26,6 +26,17 @@ client.on('messageCreate', message => {
     executeCommand(message);
 })
 
+client.on('guildMemberAdd', async member => {
+    const role = await member.guild.roles.fetch(process.env.PLAYER_ROLE_ID || '');
+
+    if (!role) {
+        logError('NO ROLE', 'on-guildMemberAdd');
+        return;
+    }
+
+    member.roles.add(role);
+})
+
 client.on('error', error => {
     logError(error, 'client');
 });
