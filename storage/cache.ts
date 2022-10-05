206dd0b1929299a5a9639f7c1d31ad3ef843dfe9
JohnnyOak85 <joao.cmp@proton.me>
2022-10-05 12:04:35 +0100

    Fixes to cache

--- storage/cache.ts
+++ storage/cache.ts
@@ -2,16 +2,64 @@ import { createClient } from 'redis';
 import { Dictionary } from '../interfaces';
 
 const client = createClient();
+const TOTAL_TRIES = 3;
 
-export const startCache = () => client.connect();
+export const startCache = async () => {
+    let counter = 0;
 
-export const getList = (key: string) => client.lRange(key, 0, -1);
-export const getMap = (key: string) => client.hGetAll(key);
-export const getValue = (key: string) => client.get(key);
-export const getListLenght = (key: string) => client.lLen(key);
+    try {
+        await client.connect();
+    } catch (error) {
+        if (counter < TOTAL_TRIES) {
+            startCache();
+            counter++;
+        }
+    }
+}
 
-export const saveList = (key: string, value: string[]) => client.rPush(key, value);
-export const saveMap = (key: string, value: Dictionary<string | number>) => client.hSet(key, value);
-export const saveValue = (key: string, value: string) => client.set(key, value);
+const ensureOpen = async () => {
+    if (!client.isOpen) {
+        await startCache();
+    }
+}
 
-export const deleteValue = (key: string) => client.del(key);
+export const getList = async (key: string) => {
+    await ensureOpen();
+
+    return client.lRange(key, 0, -1);
+}
+
+export const getMap = async (key: string) => {
+    await ensureOpen();
+    return client.hGetAll(key);
+}
+
+export const getValue = async (key: string) => {
+    await ensureOpen();
+    return client.get(key);
+}
+
+export const getListLenght = async (key: string) => {
+    await ensureOpen();
+    return client.lLen(key);
+}
+
+export const saveList = async (key: string, value: string[]) => {
+    await ensureOpen();
+    client.rPush(key, value);
+}
+
+export const saveMap = async (key: string, value: Dictionary<string | number>) => {
+    await ensureOpen();
+    return client.hSet(key, value);
+}
+
+export const saveValue = async (key: string, value: string) => {
+    await ensureOpen();
+    return client.set(key, value);
+}
+
+export const deleteValue = async (key: string) => {
+    await ensureOpen();
+    return client.del(key);
+}
